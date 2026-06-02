7e298637fea17a72aee64aa185e33ba8b681377d
martadha3li <martadha3li@gmail.com>
2026-06-02 14:46:28 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+// config.js - احفظه بهذا الاسم
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyATSMFKx4jUo_Duw0FtD5HO_M69O7A55o0",
+    authDomain: "fnews-7525e.firebaseapp.com",
+    projectId: "fnews-7525e",
+    storageBucket: "fnews-7525e.firebasestorage.app",
+    messagingSenderId: "1023530532546",
+    appId: "1:1023530532546:web:cbc1be1753237179bd455b"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
